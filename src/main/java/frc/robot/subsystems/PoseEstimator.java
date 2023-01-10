e2b75e51c8bc3776fa078501589c3c7a0b93550f
William <322969@tldsb.me>
2023-01-10 17:31:39 -0500

    added more to PoseEstimator

--- src/main/java/frc/robot/subsystems/PoseEstimator.java
+++ src/main/java/frc/robot/subsystems/PoseEstimator.java
@@ -33,23 +33,104 @@ import frc.robot.Constants.SwerveConstants;
 
 public class PoseEstimator extends SubsystemBase {
 
+  private final SwerveSubsystem swerveSubsystem;
+  private final Pigeon2Subsystem pigeon2Subsystem;
+  
+  private final PhotonCamera photonCamera = new PhotonCamera("OV5647");;
+
+  // Physical location of the camera on the robot, relative to the center of the robot.
+  private final Transform3d cameraToRobot = new Transform3d(
+    new Translation3d(Units.inchesToMeters(-12.0), 0.0, Units.inchesToMeters(-5.25)),
+    new Rotation3d());
+
+  // Ordered list of target poses by ID (WPILib is adding some functionality for this)
+  private final List<Pose3d> targetPoses = Collections.unmodifiableList(List.of(
+    new Pose3d(Units.feetToMeters(54.0), Units.feetToMeters(13.5), Units.inchesToMeters(6.0), new Rotation3d(0.0, 0.0, Units.degreesToRadians(180.0))),
+    new Pose3d(Units.feetToMeters(0.0), Units.feetToMeters(13.5), Units.inchesToMeters(6.0), new Rotation3d(0.0, 0.0, 0.0))));
+
+  // Kalman Filter Configuration. These can be "tuned-to-taste" based on how much you trust your various sensors. 
+  // Smaller numbers will cause the filter to "trust" the estimate from that particular component more than the others. 
+  // This in turn means the particualr component will have a stronger influence on the final pose estimate.
+  private final Matrix<N3, N1> stateStdDevs = VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5));
+  private final Matrix<N3, N1> visionMeasurementStdDevs = VecBuilder.fill(0.02, 0.02, Units.degreesToRadians(5));
   private static SwerveDrivePoseEstimator poseEstimator;
   
+  private final Field2d field2d = new Field2d();
 
   public PoseEstimator(SwerveSubsystem swerveSubsystem, Pigeon2Subsystem pigeon2Subsystem) {
+    this.swerveSubsystem = swerveSubsystem;
+    this.pigeon2Subsystem = pigeon2Subsystem;
+
+    poseEstimator = new SwerveDrivePoseEstimator(
+      SwerveConstants.KINEMATICS, 
+      pigeon2Subsystem.getGyroRotation(), 
+      swerveSubsystem.getPositions(), 
+      new Pose2d(new Translation2d(0, 0), new Rotation2d(0.0)), 
+      stateStdDevs, 
+      visionMeasurementStdDevs);
+
+    SmartDashboard.putData("Field", field2d);
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    
+    // Update pose estimator with visible targets
+    // latest pipeline result
+    PhotonPipelineResult res = photonCamera.getLatestResult();
+    SmartDashboard.putBoolean("Camera Has Target", res.hasTargets());
+    if(res.hasTargets()) {
+      // if there are targets record the image capture time
+      double imageCaptureTime = Timer.getFPGATimestamp() - (res.getLatencyMillis() / 1000.0);
+      PhotonTrackedTarget bestTarget = res.getBestTarget();
+      int fiducialId = bestTarget.getFiducialId();
+      if(fiducialId >= 0 && fiducialId < targetPoses.size() && bestTarget.getPoseAmbiguity() < 0.2) {
+        Pose3d targetPose = targetPoses.get(fiducialId);
+        Transform3d camToTargetTrans = bestTarget.getBestCameraToTarget();
+        Pose3d camPose = targetPose.transformBy(camToTargetTrans.inverse());
+        Pose3d robotPose = camPose.transformBy(cameraToRobot);
+        poseEstimator.addVisionMeasurement(robotPose.toPose2d(), imageCaptureTime);
+        //SmartDashboard.putString("Pose3d", robotPose.toString());
+      }
+    }
+
+    poseEstimator.updateWithTime(Timer.getFPGATimestamp(), pigeon2Subsystem.getGyroRotation(), swerveSubsystem.getPositions());
+    field2d.setRobotPose(poseEstimator.getEstimatedPosition());
+    SmartDashboard.putString("Pose", poseEstimator.getEstimatedPosition().toString());
+    // This method will be called once per scheduler run
+  }
+
+  public static Pose2d getCurrentPose() {
+    return poseEstimator.getEstimatedPosition();
+  }
+
+  public Pose2d getPose() {
+    return poseEstimator.getEstimatedPosition();
+  }
+
+  public double getPoseX() {
+    return poseEstimator.getEstimatedPosition().getX();
+  }
+
+  public double getPoseY() {
+    return poseEstimator.getEstimatedPosition().getY();
+  }
+
+  public double getPoseTheta() {
+    return poseEstimator.getEstimatedPosition().getRotation().getDegrees();
   }
 
   public Rotation2d getPoseRotation() {
     return poseEstimator.getEstimatedPosition().getRotation();
   }
 
-  public static Pose2d getCurrentPose(){
-  return poseEstimator.getEstimatedPosition();
+  public void setPose(Pose2d pose) {
+    poseEstimator.resetPosition(pigeon2Subsystem.getGyroRotation(), swerveSubsystem.getPositions(), pose);
+  }
+
+  public void setTrajectoryField2d(Trajectory trajectory) {
+    field2d.getObject("traj").setTrajectory(trajectory);
   }
 }

d30bf64be1f6ab3bbb4cc98d272039a4600eee49
HoyaRobotics <hoyarobotics@gmail.com>
2023-02-27 09:15:38 -0500

    Select position Feb 27, 2023

---
+++ src/main/java/frc/robot/subsystems/SelectPosition.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+import frc.robot.GlobalVariables;
+
+public class SelectPosition {
+    public void SelectPosition() {
+
+    }
+
+    public void rightOfPosition() {
+        GlobalVariables.leftRightPosition = GlobalVariables.leftRightPosition + 1;
+        if (GlobalVariables.leftRightPosition == 10) {
+            GlobalVariables.leftRightPosition = 1;
+        }
+    }
+
+    public void leftOfPosition() {
+        GlobalVariables.leftRightPosition = GlobalVariables.leftRightPosition - 1;
+        if (GlobalVariables.leftRightPosition == 0) {
+            GlobalVariables.leftRightPosition = 9;
+        }
+    }
+
+    public void upFromPosition() {
+        GlobalVariables.upDownPosition = GlobalVariables.upDownPosition + 1;
+        if (GlobalVariables.upDownPosition == 4) {
+            GlobalVariables.upDownPosition = 1;
+        }
+    }
+
+    public void downFromPosition() {
+        GlobalVariables.upDownPosition = GlobalVariables.upDownPosition - 1;
+        if (GlobalVariables.upDownPosition == 0) {
+            GlobalVariables.upDownPosition = 3;
+        }
+    }
+}
